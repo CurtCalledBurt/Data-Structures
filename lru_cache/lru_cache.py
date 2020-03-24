c7ff0db67e890e2fc246abecde1c9e7eda76d15c
CurtCalledBurt <CurtCalledBurt@users.noreply.github.com>
2020-03-24 15:52:34 -0700

    implemented get and set functions in lru class

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,11 @@
+from doubly_linked_list import DoublyLinkedList, ListNode
+
+# current architecture
+"""
+self.cache = [node(key, value), node(key, value), node(key, value), node(key, value), . . . ]
+self.dict = {key: node(key, value), key: node(key, value), key: node(key, value), . . . }
+"""
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +15,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.max_nodes = limit
+        self.num_current_nodes = 0
+        self.cache = DoublyLinkedList()
+        self.dict = {}
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +28,13 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        # check if key is in the dictionary
+        if key in self.dict:
+            node = self.dict[key]
+            self.cache.move_to_end(node)
+            return node.value[1]
+        else:
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +47,31 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        # if key is not in use, create a new node
+        if key not in self.dict:
+            # create a Node with key and value in DLList
+            self.cache.add_to_tail([key, value])
+            # add the node we just added to the cache tail to the dict
+            self.dict[key] = self.cache.tail
+            # increment current node count
+            self.num_current_nodes += 1
+
+        # check if key is already in use
+        else:
+            # get the node in question
+            node = self.dict[key]   
+            # update the node's value
+            node.value = [key, value]
+            # move node to back of the DLL
+            self.cache.move_to_end(node)
+
+        # if the node limit is reached delete the LRUsed
+        if self.max_nodes < self.num_current_nodes:
+            # get node to be removed
+            node = self.cache.head
+            # remove the key from dict
+            self.dict.pop(node.value[0], None)
+            # remove the node from the list
+            self.cache.remove_from_head()
+            # decrement the size counter
+            self.num_current_nodes -= 1
